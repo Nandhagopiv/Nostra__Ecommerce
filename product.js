58508b53edcf44bf1a3ecaab222651299358f23b
Nandhagopiv <nandhagopy@gmail.com>
2024-05-26 16:05:31 +0530

    Solve(Checkbox): Solved checkbox in product page

--- product.js
+++ product.js
@@ -2,16 +2,29 @@ var search = document.getElementById("search")
 var searchicon = document.getElementById("searchicon")
 var allproducts = document.getElementById("allproducts")
 var products = allproducts.querySelectorAll("h1")
-var product__theme = allproducts.querySelectorAll("h2")
-var product__color = allproducts.querySelectorAll("h3")
-var occasions = document.getElementById("occasions")
-var colors = document.getElementById("colors")
-var arrivals = document.getElementById("arrivals")
+var product__child = allproducts.querySelectorAll("h2")
+var tickbox = document.getElementsByName("tickbox")
 var off = document.getElementById("off--sec")
 var sidenav__x = document.getElementById("sidenav__x")
 var x = document.getElementById("x")
 var sidenav = document.getElementById("sidenav")
 var menubar = document.getElementById("menubar")
+var checkboxarr = []
+
+
+const checkandshow = () => {
+    for (let i = 0; i < product__child.length; i++) {
+        if (product__child[i].textContent.includes(checkboxarr[0]) || product__child[i].textContent.includes(checkboxarr[1]) || product__child[i].textContent.includes(checkboxarr[2])  || product__child[i].textContent.includes(checkboxarr[3]) || product__child[i].textContent.includes(checkboxarr[4]) || product__child[i].textContent.includes(checkboxarr[5]) || product__child[i].textContent.includes(checkboxarr[6]) || product__child[i].textContent.includes(checkboxarr[7]) || product__child[i].textContent.includes(checkboxarr[8]) || product__child[i].textContent.includes(checkboxarr[9]) || product__child[i].textContent.includes(checkboxarr[10])) {
+            product__child[i].parentElement.style.display = "block"
+        } else {
+            product__child[i].parentElement.style.display = "none"
+        }
+        
+        if (checkboxarr.length === 0) {
+            product__child[i].parentElement.style.display = "block"
+        }
+    }
+}
 
 x.addEventListener("click", function () {
     off.style.display = "none"
@@ -35,20 +48,18 @@ search.addEventListener("keyup", function () {
     }
 })
 
-var checked;
-
-for (let i = 0; i < colors.querySelectorAll("input").length; i++) {
-    colors.querySelectorAll("input")[i].addEventListener("focus", function (event) {
-        console.log(event.target.value);
-        console.log(colors.querySelectorAll("input")[0]);
-        checked = event.target.value
-        for (let i = 0; i < product__color.length; i++) {
-            if (product__color[i].textContent.includes(checked) == false) {
-                product__color[i].parentElement.style.display = "none"
-            } else {
-                product__color[i].parentElement.style.display = "block"
-            }
-
+for (let a = 0; a < tickbox.length; a++) {
+    tickbox[a].addEventListener("click",function(event){
+        
+        if (tickbox[a].checked) {
+            checkboxarr.push(event.target.value)
+            checkandshow()
+        } else {
+            var index = checkboxarr.indexOf(event.target.value)
+            checkboxarr.splice(index,1)
+            checkandshow()
         }
+        console.log(checkboxarr);
+        console.log(product__child);
     })
 }
